591c9541477879cfcfe5aa404ef624f263d81b15
Adil Nouiri <adil.nouiri@epitech.eu>
2022-01-22 16:15:39 +0100

    [add] player movement with cursor

--- Zcriou/ZCriou/Assets/Scripts/Player/PlayerMovement.cs
+++ Zcriou/ZCriou/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,22 +5,25 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-    public Animator animator;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
