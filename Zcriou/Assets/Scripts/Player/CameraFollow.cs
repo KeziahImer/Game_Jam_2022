c5b0212df718378cf8a13d0023e52c1327a28272
SimonVermeulen <simon.vermeulen@epitech.eu>
2022-01-23 05:53:09 +0100

    [add] camera following the player

---
+++ Zcriou/Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsTarget();
+    }
+
+    private void MoveTowardsTarget()
+    {
+        transform.position = new Vector3 (player.position.x, player.position.y, -5);
+    }
+
+}
